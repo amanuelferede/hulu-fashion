4a21bcccad752c9016f13842b33280e18f0cff3b
Amanuel <amanuelfrc@gmail.com>
2026-02-12 22:29:43 -0800

    'first-commit'

---
+++ app/data/colors.ts
@@ -0,0 +1,36 @@
+export const colordProducts = [
+  {
+    image: "/product/modal/c1.jpg",
+  },
+  {
+    image: "/product/modal/c2.jpg",
+  },
+  {
+    image: "/product/modal/c3.jpg",
+  },
+  {
+    image: "/product/modal/c4.jpg",
+  },
+  {
+    image: "/product/modal/c5.jpg",
+  },
+  {
+    image: "/product/modal/c6.jpg",
+  },
+  {
+    image: "/product/modal/c7.jpg",
+  },
+  {
+    image: "/product/modal/c8.jpg",
+  },
+  {
+    image: "/product/modal/c9.jpg",
+  },
+
+  {
+    image: "/product/modal/c10.jpg",
+  },
+  {
+    image: "/product/modal/c11.jpg",
+  },
+];
